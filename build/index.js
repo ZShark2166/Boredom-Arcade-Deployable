d28627351b5077680afad2efca88a4877a75ed1e
Zeeless <145777747+ZShark2166@users.noreply.github.com>
2024-11-23 20:30:19 -0600

    Added Ben's Quote

--- build/index.js
+++ build/index.js
@@ -85,7 +85,8 @@ const quotes = [
     "The best revenge is massive success. - Frank Sinatra",
     "$hit can't go down if there isn't any $hit to go down. - Zeeless",
     "Told her if i die ima die....young. - Hush",
-    'When life gives you "this page is blocked", make an unblocked site. - ab.yz'
+    'When life gives you "this page is blocked", make an unblocked site. - ab.yz',
+    "Lock every door, bar every window - there is always other ways in. - Ben_Da_Builder"
 ]
 function displayRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
