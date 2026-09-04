9917a4ecbd1f0d69ca886ee9ba57d32fcaaec196
Zeeless <zander.hudetz@gmail.com>
2026-09-04 16:23:16 -0500

    Update ads.js

--- dist/ads.js
+++ dist/ads.js
@@ -31,7 +31,8 @@ if (ADSENSE_DOMAINS.includes(domain)) {
 
 function loadAdSense() {
     const script = document.createElement("script");
-
+    const englishA = document.getElementById("english-a");
+    englishA.remove();
     script.async = true;
     script.src =
         "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2779999647710891";
